41db01e34701f0a83f1d073cf545c37469cdb838
Abbas Suterwala <abbas.s786@yahoo.co.in>
2017-07-28 20:49:21 +0530

    more testing done

--- src/UserCommandClient.java
+++ src/UserCommandClient.java
@@ -18,17 +18,20 @@ public class UserCommandClient {
     public static void main(String[] args) {
         try {
             
-            //sendCommand(GET);
             sendCommand(INC, 6900);
             sendCommand(INC, 6900);
             sendCommand(DEC, 6902);
             
+            sendCommand(INC, 6904);
+            sendCommand(DEC, 6904);
+            
+            
+            //wait for sometime to check eventually they all sync
+            System.out.println("Waiting for sometime so that the processes sync ..");
             Thread.sleep(2000);
             sendCommand(GET, 6900);
             sendCommand(GET, 6902);
-
-            
-            //sendCommand(GET);
+            sendCommand(GET, 6904);
             
         } catch(Exception e) {
             e.printStackTrace();
